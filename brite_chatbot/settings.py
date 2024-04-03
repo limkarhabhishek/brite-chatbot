b6dc84994f5164a169725ca014b3a16ae481f6f2
limkarhabhishek <abhisheklimkarh@steinnlabs.com>
2024-04-03 15:30:17 +0530

    added postgres database

--- brite_chatbot/settings.py
+++ brite_chatbot/settings.py
@@ -85,10 +85,20 @@ WSGI_APPLICATION = "brite_chatbot.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DATABASE_NAME"),
+        'USER': os.getenv("DATABASE_USERNAME"),
+        'PASSWORD': os.getenv("DATABASE_PASSWORD"),
+        'HOST': os.getenv("DATABASE_URL"),
+        'PORT': '5432'
     }
 }
 
